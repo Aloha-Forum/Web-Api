69b52ff88313009a567c592b69f179465d7636d2
Mylan Chan <mylanchan04@gmail.com>
2025-01-07 17:18:03 +0800

    feat: Update topics function

--- src/functions/topic.ts
+++ src/functions/topic.ts
@@ -6,9 +6,9 @@ async function topics(request: HttpRequest): Promise<HttpResponseInit> {
     const limit = 10;
 
     const querySpec = {
-        query: 'SELECT c.topicId, c.name, c.description \
+        query: 'SELECT c.topicId, c.name, c.description, c.popularity \
                 FROM c \
-                ORDER BY c.topicId \
+                ORDER BY c.popularity.viewCount DESC, c.popularity.postCount DESC\
                 OFFSET @offset \
                 LIMIT @limit',
         parameters: [
@@ -19,6 +19,21 @@ async function topics(request: HttpRequest): Promise<HttpResponseInit> {
 
     try {
         const { resources: items } = await Aloha.Topic.items.query(querySpec).fetchAll();
+
+        for (let item of items) {
+            const querySpec = {
+                query: 'SELECT TOP 1 c.lastActivity \
+                        FROM c \
+                        WHERE c.topicId = @topicId \
+                        ORDER BY c.postAt DESC',
+                parameters: [
+                    { name: '@topicId', value: item.topicId }
+                ],
+            };
+            const { resources: [post] } = await Aloha.Post.items.query(querySpec).fetchNext();
+            item.lastActivity = post ? post.lastActivity : -1;
+        }
+
         return { body: JSON.stringify(items) };
     }
     catch (error) {
