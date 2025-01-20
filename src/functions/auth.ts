40245d4d7143ce87cf71840802a361be75adab41
Mylan Chan <mylanchan04@gmail.com>
2025-01-20 23:23:38 +0800

    fix: Duplicated session and Incorrect return type

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -4,6 +4,21 @@ import { verifyToken } from "../utils/auth";
 import { RequestFormatError, ResourceNotFoundError } from "../shared/error";
 import { getUidByEmail } from "../utils/user";
 
+async function isSessionExisted(token: String): Promise<boolean> {
+
+    const querySpec = {
+        query: 'SELECT VALUE COUNT(1) \
+                FROM c \
+                WHERE c.sid = @token',
+        parameters: [
+            { name: '@token', value: token }
+        ],
+    };
+
+    const { resources: [count] } = await Aloha.Session.items.query(querySpec).fetchAll();
+    return count > 0
+}
+
 async function login(request: HttpRequest): Promise<HttpResponseInit> {
     try {
         const accessToken = request.headers.get('authorization');
@@ -13,10 +28,12 @@ async function login(request: HttpRequest): Promise<HttpResponseInit> {
         const user = await getUidByEmail(respsonse.email);
         if (user == null) throw new ResourceNotFoundError()
     
-        const session = { uid: user.uid, sid: accessToken, ttl: respsonse.expiresIn}
-        await Aloha.Session.items.create(session)
+        if (!await isSessionExisted(accessToken)) {
+            const session = { uid: user.uid, sid: accessToken, ttl: respsonse.expiresIn}
+            await Aloha.Session.items.create(session)
+        }
     
-        return { body: JSON.stringify({uid: user.uid, email: respsonse.email }) }
+        return { status: 202, body: JSON.stringify({uid: user.uid, email: respsonse.email }) }
     }
     catch (error) {
         return { status: error.statusCode || 500 }
@@ -38,7 +55,7 @@ async function signup(request: HttpRequest): Promise<any> {
         const session = { uid: uid, sid: accessToken, ttl: 3600}
         await Aloha.Session.items.create(session)
     
-        return { status: 202, body: { uid: uid, email: respsonse.email } }
+        return { status: 202, body: JSON.stringify({uid: uid, email: respsonse.email }) }
     }
     catch (error) {
         return { status: error.statusCode || 500 }
