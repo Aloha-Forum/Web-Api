30640ad8fd8bb3cbf6cd0eda25312b3f35cc1dc9
Mylan Chan <mylanchan04@gmail.com>
2025-01-28 14:55:24 +0800

    feat: Recommend posts

---
+++ src/functions/recommend.ts
@@ -0,0 +1,38 @@
+import { app, HttpRequest, HttpResponseInit } from "@azure/functions";
+import { Aloha } from "../shared/container";
+import { getParams } from "../utils/validate";
+import { ResourceNotFoundError } from "../shared/error";
+
+async function recommnedPosts(request: HttpRequest): Promise<HttpResponseInit> {
+    try {            
+        const page = parseInt(request.query.get('page')) || 0;
+        const limit = 10;
+
+        const querySpec = {
+            query: 'SELECT c.postId, c.uid, c.title, LEFT(c.body, 50) as body, c.lastActivity, c.popularity \
+                    FROM c \
+                    ORDER BY c.popularity.viewCount, c.popularity.commentCount, c.postAt DESC \
+                    OFFSET @offset \
+                    LIMIT @limit',
+            parameters: [
+                { name: '@offset', value: page*limit },
+                { name: '@limit', value: limit }
+            ],
+        };
+        const { resources: items } = await Aloha.Post.items.query(querySpec).fetchAll();
+
+        if (items.length) 
+            return { body: JSON.stringify(items) }
+        else
+            throw new ResourceNotFoundError();
+    }
+    catch (error) {
+        return { status: error.statusCode || 500, body: error.message };
+    }
+}
+
+app.http('recommendPosts', {
+    methods: ['GET'],
+    route: 'api/recommend',
+    handler: recommnedPosts
+});
